b5299d9365f6b87642bc7f4a120e517d1ab0ee2a
wryser <104174626+wryser@users.noreply.github.com>
2022-05-14 12:59:59 +1000

    Add files via upload

---
+++ Components/BakaRemove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BakaRemove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    float destroybakatime = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        destroybakatime -= Time.deltaTime;
+        if (destroybakatime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
